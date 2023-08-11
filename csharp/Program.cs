50e0c319406d5dd78b78f10d2e7755ace2179f79
Felipe Bezerra Lima <71532053+flipbezerra@users.noreply.github.com>
2023-08-11 09:36:18 -0500

    limpeza

--- csharp/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-var nomes = new[] {"Felipe", "João", "Maria"};
-foreach(var nome in nomes) {
-    Console.WriteLine($"Hello, {nome}!");
-}
-
