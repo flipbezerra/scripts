a6ee7ab8d7fcc132b041ad7917d929d7d8acbc5b
Felipe Bezerra Lima <71532053+Bezerha@users.noreply.github.com>
2023-09-26 07:09:00 -0500

    feat: adicionar script python para mudar metadados de um arquivo

---
+++ python/metadata.py
@@ -0,0 +1,32 @@
+import os
+import time
+import datetime
+import piexif
+
+fileLocation = r'C:\Users\Felipe\Pictures\NSFW\ZONE'
+
+# def noMeta(file):
+#   print("gutting exif data from {}".format(file))
+#   piexif.remove(file)
+
+# for root, dir, files in os.walk(fileLocation):
+#   for file in files:
+#     if file.lower().endswith(".jpg"):
+#       noMeta(os.path.join(root, file))
+
+year = 2017
+month = 11
+day = 1
+hour = 19
+minute = 50
+second = 0
+for filename in os.listdir(fileLocation):
+    f = os.path.join(fileLocation, filename)
+    # checking if it is a file
+    if os.path.isfile(f):
+        print(f)
+    date = datetime.datetime(year=year, month=month, day=day, hour=hour, minute=minute, second=second)
+    day+=1
+    modTime = time.mktime(date.timetuple())
+
+    os.utime(f, (modTime, modTime))
