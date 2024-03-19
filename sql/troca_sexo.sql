dd1001a44d5a81a3af0552e6767d448e3808a05b
flipbezerra <felipebezerralima04@gmail.com>
2024-03-19 07:35:13 -0500

    refactor: alteração na formatação da query troca_sexo.sql

--- sql/troca_sexo.sql
+++ sql/troca_sexo.sql
@@ -1,4 +1,10 @@
 -- troca na tabela salário o atributo sexo (enum('m', 'f')) por 'm' onde ele for 'f' e vice-versa
+
+INSERT INTO salarios (id, nome, sexo, salario) VALUES (1,'A', 'f', 2500);
+INSERT INTO salarios (id, nome, sexo, salario) VALUES (2,'B', 'f', 1500);
+INSERT INTO salarios (id, nome, sexo, salario) VALUES (3,'C', 'm', 5500);
+INSERT INTO salarios (id, nome, sexo, salario) VALUES (4,'D', 'm', 500);
+
 UPDATE salarios 
 SET sexo = IF(sexo = 'm', 'f', IF(sexo = 'f', 'm', sexo)) 
 WHERE id <> 0;
@@ -9,8 +15,3 @@ SET sexo = CASE sexo
     WHEN 'f' THEN 'm'
     ELSE sexo
 END WHERE id <> 0;
-
-INSERT INTO salarios (id, nome, sexo, salario) VALUES (1,'A', 'f', 2500);
-INSERT INTO salarios (id, nome, sexo, salario) VALUES (2,'B', 'f', 1500);
-INSERT INTO salarios (id, nome, sexo, salario) VALUES (3,'C', 'm', 5500);
-INSERT INTO salarios (id, nome, sexo, salario) VALUES (4,'D', 'm', 500);
